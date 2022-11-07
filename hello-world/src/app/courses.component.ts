08eb3b696808a9f677e483142878617f5185752b
Tirthankar Bhattacharya <tirthankar.bhattacharya@zeotap.com>
2022-11-07 11:43:23 +0530

    class binding

--- hello-world/src/app/courses.component.ts
+++ hello-world/src/app/courses.component.ts
@@ -4,14 +4,9 @@ import { CoursesService } from "./course/courses.service";
 @Component({
     selector: 'courses',
     template: `
-        <table>
-            <<tr>
-                <td [attr.colspan]="colSpan"></td>
-            </tr>
-        </table>
+        <button class="btn btn-primary" [class.active]="isActive">Save</button>
     `
 })
 export class CoursesComponent{
-    imageUrl = "https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/https://coursera-course-photos.s3.amazonaws.com/cb/3c4030d65011e682d8b14e2f0915fa/shutterstock_226881610.jpg?auto=format%2Ccompress&dpr=1";
-    colSpan = 2;
+    isActive = true;
 }
