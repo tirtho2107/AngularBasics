c051f619a3228f8f6b03e3308f6a541c292281a3
Tirthankar Bhattacharya <tirthankar.bhattacharya@zeotap.com>
2022-11-07 11:17:38 +0530

    using attr for dom attributes not in html

--- hello-world/src/app/courses.component.ts
+++ hello-world/src/app/courses.component.ts
@@ -3,22 +3,15 @@ import { CoursesService } from "./course/courses.service";
 
 @Component({
     selector: 'courses',
-    template: `<h2>{{ title }}</h2>
-                <h3 [textContent]="title"></h3>
-                <img [src]="imageUrl"/>
-                <ul>
-                    <li *ngFor="let course of courses">
-                        {{ course }}
-                    </li>
-                </ul>
+    template: `
+        <table>
+            <<tr>
+                <td [attr.colspan]="colSpan"></td>
+            </tr>
+        </table>
     `
 })
 export class CoursesComponent{
-    title = "List of courses";
-    imageUrl = "https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/https://coursera-course-photos.s3.amazonaws.com/cb/3c4030d65011e682d8b14e2f0915fa/shutterstock_226881610.jpg?auto=format%2Ccompress&dpr=1"
-    courses;
-    
-    constructor(service: CoursesService) {
-        this.courses = service.getCourses();
-    }
+    imageUrl = "https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/https://coursera-course-photos.s3.amazonaws.com/cb/3c4030d65011e682d8b14e2f0915fa/shutterstock_226881610.jpg?auto=format%2Ccompress&dpr=1";
+    colSpan = 2;
 }
