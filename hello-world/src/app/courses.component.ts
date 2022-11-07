2fa8bc72cdfa984ca96fb8c8628ad40bf2bc84dc
Tirthankar Bhattacharya <tirthankar.bhattacharya@zeotap.com>
2022-11-07 10:28:46 +0530

    adding a service

--- hello-world/src/app/courses.component.ts
+++ hello-world/src/app/courses.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { CoursesService } from "./course/courses.service";
 
 @Component({
     selector: 'courses',
@@ -12,5 +13,10 @@ import { Component } from "@angular/core";
 })
 export class CoursesComponent{
     title = "List of courses";
-    courses = ["course1", "course2", "course3"];
+    courses;
+    
+    constructor() {
+        let service = new CoursesService();
+        this.courses = service.getCourses();
+    }
 }
