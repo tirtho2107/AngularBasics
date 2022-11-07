eaacd259186e5674fd5b6a7afe1a4f82175af202
Tirthankar Bhattacharya <tirthankar.bhattacharya@zeotap.com>
2022-11-07 14:19:06 +0530

    two way binding

--- hello-world/src/app/app.module.ts
+++ hello-world/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -15,7 +16,8 @@ import { CoursesService } from './course/courses.service';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [
     CoursesService
