ae1ecdb06bfee1d4c3f2cc6136919c81eeab1812
Tirthankar Bhattacharya <tirthankar.bhattacharya@zeotap.com>
2022-11-07 10:09:36 +0530

    using directives

--- hello-world/src/app/courses.component.ts
+++ hello-world/src/app/courses.component.ts
@@ -2,12 +2,15 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'courses',
-    template: '<h2>{{ "Title:" + getTitle() }}</h2>'
+    template: `<h2>{{ title }}</h2>
+                <ul>
+                    <li *ngFor="let course of courses">
+                        {{ course }}
+                    </li>
+                </ul>
+    `
 })
 export class CoursesComponent{
     title = "List of courses";
-
-    getTitle(){
-        return this.title;
-    }
+    courses = ["course1", "course2", "course3"];
 }
