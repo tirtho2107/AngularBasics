b300e2abddd2afbc6f89e17636b08811442b48af
Tirthankar Bhattacharya <tirthankar.bhattacharya@zeotap.com>
2022-11-07 12:58:50 +0530

    style binding

--- hello-world/src/app/courses.component.ts
+++ hello-world/src/app/courses.component.ts
@@ -4,9 +4,9 @@ import { CoursesService } from "./course/courses.service";
 @Component({
     selector: 'courses',
     template: `
-        <button class="btn btn-primary" [class.active]="isActive">Save</button>
+        <button [style.backgroundColor]="isActive ? 'blue' : 'white'">Save</button>
     `
 })
 export class CoursesComponent{
-    isActive = true;
+    isActive = false;
 }
